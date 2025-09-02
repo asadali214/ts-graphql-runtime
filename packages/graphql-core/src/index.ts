856003bfe8bc2809e1f4137e3d2e71b2799a3840
Asad Ali <14asadali@gmail.com>
2025-09-02 18:16:07 +0500

    fix: file namings

--- packages/graphql-core/src/index.ts
+++ packages/graphql-core/src/index.ts
@@ -1,4 +1,4 @@
-export { GraphQLClient } from './graphQLClients';
-export { GraphQLClientConfig } from './graphQLClientConfigs';
+export { GraphQLClient } from './graphQLClient';
+export { GraphQLClientConfig } from './graphQLClientConfig';
 export { GraphQLResponse } from './graphQLResponse';
 export { AbortError } from './abortError';
